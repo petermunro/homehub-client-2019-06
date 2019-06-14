81187f1c42767811cd978efe4118db1023b8e589
Peter Munro <peter@petermunro.org>
2019-06-14 23:08:54 +0100

    Add query batching

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { ApolloClient } from "apollo-client";
-import { HttpLink } from "apollo-link-http";
+import { BatchHttpLink } from "apollo-link-batch-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
 import { IntrospectionFragmentMatcher } from "apollo-cache-inmemory";
@@ -15,7 +15,7 @@ const fragmentMatcher = new IntrospectionFragmentMatcher({
 });
 
 const client = new ApolloClient({
-  link: new HttpLink({
+  link: new BatchHttpLink({
     uri: "http://localhost:4001/graphql"
   }),
   cache: new InMemoryCache({ fragmentMatcher })
